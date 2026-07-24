d5c5df4b69e54555829e1c22ce036bbb3e0d064b
Admin <heeyoung.vox@gmail.com>
2026-07-24 07:39:17 +0700

    fix check border

--- src/main/java/heeyoung/soldier/service/PhysicsSimulationService.java
+++ src/main/java/heeyoung/soldier/service/PhysicsSimulationService.java
@@ -14,14 +14,14 @@ public class PhysicsSimulationService {
         double dy = player.getPlayerInput().getDy() * PLAYER_SPEED;
 
         // check border
-        if (player.getX() + dx < GameWorld.MAP_WIDTH || player.getX() + dx > 0)
+        if (player.getX() + dx < GameWorld.MAP_WIDTH && player.getX() + dx > 0)
             player.setX(player.getX() + dx);
         else if (player.getX() + dx >= GameWorld.MAP_WIDTH)
             player.setX(GameWorld.MAP_WIDTH);
         else if (player.getX() + dx <= 0)
             player.setX(0);
 
-        if (player.getY() + dy < GameWorld.MAP_HEIGHT || player.getY() + dy > 0)
+        if (player.getY() + dy < GameWorld.MAP_HEIGHT && player.getY() + dy > 0)
             player.setY(player.getY() + dy);
         else if (player.getY() + dy >= GameWorld.MAP_HEIGHT)
             player.setY(GameWorld.MAP_HEIGHT);
