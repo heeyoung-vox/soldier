22ef6132e90d849a1c1e378b668100223507d7c1
Admin <heeyoung.vox@gmail.com>
2026-07-24 07:18:31 +0700

    fix border simulation

--- src/main/java/heeyoung/soldier/service/PhysicsSimulationService.java
+++ src/main/java/heeyoung/soldier/service/PhysicsSimulationService.java
@@ -12,9 +12,20 @@ public class PhysicsSimulationService {
     public void simulate(Player player) {
         double dx = player.getPlayerInput().getDx() * PLAYER_SPEED;
         double dy = player.getPlayerInput().getDy() * PLAYER_SPEED;
+
+        // check border
         if (player.getX() + dx < GameWorld.MAP_WIDTH || player.getX() + dx > 0)
             player.setX(player.getX() + dx);
+        else if (player.getX() + dx >= GameWorld.MAP_WIDTH)
+            player.setX(GameWorld.MAP_WIDTH);
+        else if (player.getX() + dx <= 0)
+            player.setX(0);
+
         if (player.getY() + dy < GameWorld.MAP_HEIGHT || player.getY() + dy > 0)
             player.setY(player.getY() + dy);
+        else if (player.getY() + dy >= GameWorld.MAP_HEIGHT)
+            player.setY(GameWorld.MAP_HEIGHT);
+        else if (player.getY() + dy <= 0)
+            player.setY(0);
     }
 }
